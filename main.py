e1b34b5717f735a1b7446c192346d0e3b259de32
Gary <kaka22-kiodok@web.de>
2021-04-07 21:55:26 +0200

    feat(profile): Added welcome message and first steps for registration

--- main.py
+++ main.py
@@ -1,14 +1,85 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+chat_id = ''
+
+questions = [
+    "Wie lautet dein Geburtsdatum? (DD.MM.YYYY)",
+    "Was fährst du für ein Auto?"
+]
+question_counter = 0
+
+registration_data = {
+    "id": "",
+    "birthday": "",
+    "car": "",
+    "link": "",
+    "name": "",
+    "traveler": False,
+    "driver": False
+}
 
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+def next_question(question_index):
+    question = questions[question_index]
+    return question
 
 
-updater = Updater('1783162081:AAGSSI5KySI1xb1wXRmIte-x58sBR9DzbO4')
+def reply(update, context):
+    user_input = update.message.text
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    global question_counter
+    global registration_data
 
-updater.start_polling()
-updater.idle()
+    if question_counter == 0:
+        registration_data["name"] = user_input
+    elif question_counter == 1:
+        registration_data["birthday"] = user_input
+    else:
+        registration_data["car"] = user_input
+
+    if question_counter < 2:
+        update.message.reply_text(next_question(question_counter))
+        question_counter += 1
+    else:
+        question_counter += 1
+
+
+def start_chat(update: Update, context: CallbackContext):
+    global chat_id
+    chat_id = update.message.chat.id
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Guten Tag {update.effective_user.first_name}, \n'
+             f'ich bin deine persönliche Mitfahrzentrale "likeuber"! \n\n'
+             f'Um die Mitfahrzentrale nutzen zu können, musst du dich vorerst registrieren, dafür benötige ich diverse Daten von dir. \n\n'
+             f'Wie lautet dein Name als Fahrer/Mitfahrer?'
+    )
+
+
+def main():
+    bot = Updater(token='1783162081:AAGSSI5KySI1xb1wXRmIte-x58sBR9DzbO4', use_context=True)
+
+    bot.dispatcher.add_handler(CommandHandler('start', start_chat))
+
+    questions_handler = MessageHandler(Filters.text, reply)
+
+    bot.dispatcher.add_handler(questions_handler)
+
+    bot.start_polling()
+
+    global question_counter
+    while question_counter < 4:
+        if question_counter == 3:
+            bot.dispatcher.remove_handler(questions_handler)
+            question_counter += 1
+
+            bot.bot.sendMessage(chat_id=chat_id, text='Danke für deine Registrierung!')
+            print(registration_data)
+
+    bot.idle()
+
+
+if __name__ == "__main__":
+    main()
