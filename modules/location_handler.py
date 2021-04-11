d83806f84aedfb54ba42cc25b7220be079666a00
Julian Yaman <julianyaman@posteo.eu>
2021-04-11 13:01:26 +0200

    docs: location_handler.py, start_menu.py, util.py

--- modules/location_handler.py
+++ modules/location_handler.py
@@ -3,58 +3,116 @@ from modules.util import haversine
 
 
 def get_driver_destination(user_id):
+    """Gets the destination of the driver with the given user_id
+
+    Args:
+        user_id (int): The ID of the user
+
+    Returns:
+        dist: Coordinates dictionary containing the longitude and the latitude of the destination of the driver
+    """
+
     result = None
+    # Opens the location data storage file in READ mode and searches for the driver and the related destination object
+    # by using a for loop.
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
 
             for location_dataset in data:
+                # If the user has the specified ID, is a driver and has "is_destination" set to True in the
+                # current object, then the coordinates of the driver's destination are returned
                 if location_dataset["user_id"] == user_id and location_dataset["driver"] and location_dataset["is_destination"]:
                     result = location_dataset["coordinates"]
 
             return result
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
 
 
 def get_distance_from_drivers(coordinates):
+    """Returns the result of calculating the distance from one point on a sphere to another in kilometers by using the
+    haversine formula
+
+    Args:
+        coordinates (dict): The coordinates dictionary containing the longitude and the latitude *of the passenger*
+
+    Returns:
+        list: List of dictionaries containing the user_id of the driver and the distance to the driver
+    """
+
     results = []
+    # Opens the location data storage file in READ mode and searches for all dateset of
+    # drivers with their current location
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
 
             for location_dataset in data:
+                # If the user is a driver and has "is_current_location" set to True,
+                # then the coordinates of the passenger and the one of the driver will be used to calculate the distance
+                # by using the [Haversine formula](https://en.wikipedia.org/wiki/Haversine_formula)
                 if location_dataset["driver"] and location_dataset["is_current_location"]:
+                    # Calculating the distance - function can be found in modules.util
                     distance = haversine(coordinates["longitude"], coordinates["latitude"],
                                          location_dataset["coordinates"]["longitude"],
                                          location_dataset["coordinates"]["latitude"])
+
+                    # Appends dictionaries to the array list containing all drivers and their distance to the user
                     results.append({"distance": distance, "user_id": location_dataset["user_id"]})
 
             return results
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
 
 
 def set_driver_current_location(user_id, coordinates):
+    """Sets and updates the current live location of the driver in the location data storage
+
+    Args:
+        user_id (int): The user ID of the driver
+        coordinates (dict): The coordinates dictionary containing the longitude and the latitude *of the driver*
+
+    Returns:
+        dict: Returns a success object containing the information if the action was successful
+    """
+
+    # Opens the location data storage file first in READ mode and then checks if the a "current location" already
+    # exists in the JSON file or if a new entry must be made
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
+
+            # Loops through all objects in the array and looking for all drivers
+            # of which their live location is currently saved in the file
             for location_dataset in data:
-                if location_dataset["user_id"] == user_id and location_dataset["is_current_location"]:
+                if location_dataset["user_id"] == user_id and location_dataset["driver"] and \
+                        location_dataset["is_current_location"]:
+
+                    # Updating the coordinates value of the key "coordinates" in the local variable
                     location_dataset["coordinates"] = coordinates
+
+                    # Writing into the file with the updated list of location data
                     with open('data/location.json', 'w') as file:
                         try:
                             json.dump(data, file, sort_keys=True, indent=4)
                             return {"error": False, "type": "UpdateLocationSuccess"}
                         except:
+                            # If the file could not be opened, it will return an error
+                            # dictionary with a custom error type
                             print("Error occurred when trying to open the file location.json")
                             return {"error": True, "type": "JSONFileError"}
 
+            # If no "current location" entry could be found, then a new one will be created by opening the file in
+            # WRITE mode and dumping a new dataset into it.
             with open('data/location.json', 'w') as file:
 
                 try:
+                    # Creating a new dataset which will then be dumped into the file
                     new_dataset = {"user_id": user_id, "driver": True, "passenger": False,
                                    "coordinates": coordinates, "is_current_location": True, "is_destination": False}
 
@@ -63,31 +121,52 @@ def set_driver_current_location(user_id, coordinates):
 
                     return {"error": False, "type": "SetLocationSuccess"}
                 except:
+                    # If the file could not be opened, it will return an error dictionary with a custom error type
                     print("Error occurred when trying to open the file location.json")
                     return {"error": True, "type": "JSONFileError"}
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
 
 
 def set_driver_destination(user_id, destination):
+    """Sets the destination of a new driver with a given user_id
+
+    Args:
+        user_id (int): The ID of the user
+        destination (dist): The destination containing the longitude and the latitude of the destination
+
+    Returns:
+        dist: Returns a success object containing the information if the action was successful
+    """
+
+    # Opens the location data storage file first in READ mode and then checks if there is already a destination
+    # (highly unlikely)  in the JSON file or if it needs to create a new destination entry
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
+
+            # Loops through all objects in the array and looking for all drivers
+            # of which their destination is currently saved in the file (highly unlikely to happen)
             for location_dataset in data:
-                if location_dataset["user_id"] == user_id  and location_dataset["is_destination"]:
+                if location_dataset["user_id"] == user_id and location_dataset["driver"] \
+                        and location_dataset["is_destination"]:
                     with open('data/location.json', 'w') as file:
                         try:
                             location_dataset["coordinates"] = destination
                             json.dump(data, file, sort_keys=True, indent=4)
                             return {"error": False, "type": "UpdateDestinationSuccess"}
                         except:
+                            # If the file could not be opened, it will return an error dictionary with a custom error type
                             print("Error occurred when trying to open the file location.json")
                             return {"error": True, "type": "JSONFileError"}
 
+            # Writing the given destination into the location storage file
             with open('data/location.json', 'w') as file:
 
                 try:
+                    # Creating a new destination dataset which will then be dumped into the file
                     new_dataset = {"user_id": user_id, "driver": True, "passenger": False, "coordinates": destination,
                                    "is_current_location": False, "is_destination": True}
 
@@ -96,31 +175,58 @@ def set_driver_destination(user_id, destination):
 
                     return {"error": False, "type": "SetDestinationSuccess"}
                 except:
+                    # If the file could not be opened, it will return an error dictionary with a custom error type
                     print("Error occurred when trying to open the file location.json")
                     return {"error": True, "type": "JSONFileError"}
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
 
 
 def set_passenger_current_location(user_id, coordinates):
+    """Sets or updates the current location of a passenger
+
+    Args:
+        user_id (int): The ID of the user
+        coordinates (dist): The coordinates containing the longitude and the latitude of the current live location
+
+    Returns:
+        dist: Returns a success object containing the information if the action was successful
+    """
+
+    # Opens the location data storage file first in READ mode and then checks if there is already a object containing
+    # the current live location of the passenger or if a new passenger entry must be added
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
+
+            # Loops through all objects in the array and looking for all passengers
+            # of which their live location was already saved in the file
             for location_dataset in data:
-                if location_dataset["user_id"] == user_id and location_dataset["is_current_location"]:
+                if location_dataset["user_id"] == user_id and location_dataset["passenger"] \
+                        and location_dataset["is_current_location"]:
+
+                    # Updating the coordinates value of the key "coordinates" in the local variable
                     location_dataset["coordinates"] = coordinates
+
+                    # Writing into the file with the updated list of location data
                     with open('data/location.json', 'w') as file:
                         try:
                             json.dump(data, file, sort_keys=True, indent=4)
                             return {"error": False, "type": "UpdateLocationSuccess"}
                         except:
+                            # If the file could not be opened, it will return an error
+                            # dictionary with a custom error type
                             print("Error occurred when trying to open the file location.json")
                             return {"error": True, "type": "JSONFileError"}
 
+            # If no "current location" entry could be found, then a new one will be created by opening the file in
+            # WRITE mode and dumping a new dataset into it.
             with open('data/location.json', 'w') as file:
 
                 try:
+                    # Creating a new dataset which will then be dumped into the file
                     new_dataset = {"user_id": user_id, "driver": False, "passenger": True,
                                    "coordinates": coordinates, "destination": "",
                                    "is_current_location": True, "is_destination": False}
@@ -130,29 +236,49 @@ def set_passenger_current_location(user_id, coordinates):
 
                     return {"error": False, "type": "SetLocationSuccess"}
                 except:
+                    # If the file could not be opened, it will return an error dictionary with a custom error type
                     print("Error occurred when trying to open the file location.json")
                     return {"error": True, "type": "JSONFileError"}
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
 
 
 def delete_user_location_data(user_id):
+    """Deletes all the location data saved from a user with the given user ID
+
+    Args:
+        user_id (int): The ID of the user
+
+    Returns:
+        dist: Returns a success object containing the information if the action was successful
+    """
     with open('data/location.json', 'r') as location_collection:
         try:
             data = json.load(location_collection)
             new_data = []
+
+            # Looping through all the data and appending all data which DOES NOT belong to the
+            # user with the give user_id
             for location_dataset in data:
                 if location_dataset["user_id"] != user_id:
+                    # the new list contains all data except the data from the user with the given user_id
                     new_data.append(location_dataset)
+
             data = new_data
+
+            # Writing the new data list into the location file storage
             with open('data/location.json', 'w') as file:
                 try:
                     json.dump(data, file, sort_keys=True, indent=4)
                 except:
+                    # If the file could not be opened, it will return an error dictionary with a custom error type
                     print("Error occurred when trying to open the file location.json")
                     return {"error": True, "type": "JSONFileError"}
+
             return {"error": False, "type": "NoDataFound"}
         except:
+            # If the file could not be opened, it will return an error dictionary with a custom error type
             print("Error occurred when trying to open the file location.json")
             return {"error": True, "type": "JSONFileError"}
--- modules/util.py
+++ modules/util.py
@@ -6,24 +6,45 @@ from math import radians, cos, sin, asin, sqrt
 
 
 def haversine(lon1, lat1, lon2, lat2):
+    """ Calculate the great circle distance between two points on the earth (specified in decimal degrees)
+
+    Args:
+        lon1 (float): The longitude of the first coordinate
+        lat1 (float): The latitude of the first coordinate
+        lon2 (float): The longitude of the second coordinate
+        lat2 (float): The latitude of the second coordinate
+
+
+    Returns:
+        float: Distance between two given coordinates
     """
-    https://stackoverflow.com/questions/42686300/how-to-check-if-coordinate-inside-certain-area-python
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
+
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine formula
+    # [Haversine Formula](https://en.wikipedia.org/wiki/Haversine_formula)
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371  # Radius of earth in kilometers
     return c * r
 
 
 def build_button_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    """ Preparing a list for being used in a InlineKeyboardMarkup
+
+    Args:
+        buttons (list): The longitude of the first coordinate
+        n_cols (int): n_cols = 1 is for single column and other values for multiple rows
+        header_buttons: Header Buttons
+        footer_buttons: Footer Buttons
+
+
+    Returns:
+        list: Prepared list for the InlineKeyboardMarkup
+    """
+
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
     if header_buttons:
         menu.insert(0, header_buttons)
@@ -32,10 +53,19 @@ def build_button_menu(buttons, n_cols, header_buttons=None, footer_buttons=None)
     return menu
 
 
-def get_user_age(birthday):
-    birth_date = datetime.date(2000, 2, 1)
+def get_user_age(birthday_str):
+    """Returns the rounded off by calculating the difference between the birthday and today
 
-    time_difference = datetime.date.today() - birth_date
+    Args:
+        birthday_str (str): The birthday as a string
+
+    Returns:
+        float: Rounded age
+    """
+
+    birthday = datetime.datetime.strptime(birthday_str, "%d.%m.%Y")
+
+    time_difference = datetime.datetime.today() - birthday
 
     age = "%.0f" % (time_difference.days / 365)
     return age
